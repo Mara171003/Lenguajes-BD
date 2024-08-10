66d59d81fd1fad03c0d85f7e74d829e0ffe5ee1a
Daniel <124007735+DanielBC1232@users.noreply.github.com>
2024-08-10 16:52:15 -0600

    Login
    prueba con php y OCI8 para hacer login (incompleto)

--- Proyecto/usuario/login.php
+++ Proyecto/usuario/login.php
@@ -18,6 +18,38 @@ if (!empty($_POST["btnIngresar"])) {
         oci_bind_by_name($stid, ':password', $password);
         oci_execute($stid);
 
+    
+        while (($datos = oci_fetch_object($stid)) != false) { // Si se encuentra un resultado
+            $_SESSION['usuario'] = $datos->CORREO; // Extrae el correo
+            $_SESSION['id'] = $datos->ID_USUARIO; // Extrae el id del usuario autenticado
+            $_SESSION['rol'] = $datos->ID_ROL; // Extrae el rol
+            $_SESSION['apellido1'] = $datos->PRIMER_APELLIDO;
+            $_SESSION['apellido2'] = $datos->SEGUNDO_APELLIDO;
+            $_SESSION['nombre'] = $datos->NOMBRE;
+            echo "success";
+            header("Location: ../index.php");
+        }
+    
+    }
+}
+
+die(); //-- debajo de este die() esta el codigo anterior (solo cambia el while x el if)
+
+if (!empty($_POST["btnIngresar"])) {
+    if (!empty($_POST["correo"]) && !empty($_POST["password"])) {
+        $correo = recogePost('correo');
+        $password = recogePost('password');
+        $password = md5($password);
+
+        $conn = Conecta(); // Obtener la conexión a la base de datos
+
+        // Preparar y ejecutar la consulta
+        $query = "SELECT * FROM usuario WHERE correo = :correo AND password = :password";
+        $stid = oci_parse($conn, $query);
+        oci_bind_by_name($stid, ':correo', $correo);
+        oci_bind_by_name($stid, ':password', $password);
+        oci_execute($stid);
+
         if ($datos = oci_fetch_object($stid)) { // Si se encuentra un resultado
             $_SESSION['usuario'] = $datos->CORREO; // Extrae el correo
             $_SESSION['id'] = $datos->ID_USUARIO; // Extrae el id del usuario autenticado
